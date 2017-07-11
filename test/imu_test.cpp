0e741ba3ee577df6ad989acf552ace697fe24f9c
Cameron McQuinn <cameron.mcquinn@gmail.com>
2017-07-11 13:21:49 -0600

    Wrote an imu calibration test

---
+++ test/imu_test.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include "rosflight.h"
+#include "test_board.h"
+#include "cmath"
+
+#define EXPECT_OPPOSITE(x, y) EXPECT_LE(std::abs(x) - std::abs(y), 0.001)
+
+using namespace rosflight_firmware;
+
+TEST(imu_test, calibration_simulation)
+{
+    testBoard board;
+    ROSflight rf(board);
+
+    // Initialize firmware
+    rf.init();
+
+    float fake_accel[3] = {1, 1.2, 0.8};
+    float dummy_gyro[3] = {0, 0, 0};
+
+    // Feed in fake acceleration data
+    board.set_imu(fake_accel, dummy_gyro, (uint64_t)(t*1e6));
+
+    // Run the firmware
+    rf.run();
+
+    // When does the calibration happen?
+
+    float bias[3];
+
+    // Get the bias values
+    bias[0] = rf.params_.get_param_float(ACC_X_BIAS);
+    bias[1] = rf.params_.get_param_float(ACC_Y_BIAS);
+    bias[2] = rf.params_.get_param_float(ACC_Z_BIAS);
+
+    for (int i = 0; i < 3; i++)
+    {
+      EXPECT_OPPOSITE(fake_accel[i], bias[i]);
+    }    
+}
