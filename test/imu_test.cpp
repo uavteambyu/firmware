4734ba6d3cbdd703ab030a6b89c3e6a7a7d21932
Cameron McQuinn <cmcquinn@user.noreply.github.com>
2017-07-13 10:42:19 -0600

    Fixed naming issues in imu_test

--- test/imu_test.cpp
+++ test/imu_test.cpp
@@ -18,20 +18,22 @@ TEST(imu_test, calibration_simulation)
     float fake_accel[3] = {1, 1.2, 0.8};
     float dummy_gyro[3] = {0, 0, 0};
 
-    // Feed in fake acceleration data
-    board.set_imu(fake_accel, dummy_gyro, (uint64_t)(t*1e6));
+    for (int t = 0; t < 100; ++t) {
+        // Feed in fake acceleration data
+        board.set_imu(fake_accel, dummy_gyro, (uint64_t)(t*1e6));
 
-    // Run the firmware
-    rf.run();
+        // Run the firmware
+        rf.run();
 
+    }
     // When does the calibration happen?
 
     float bias[3];
 
     // Get the bias values
-    bias[0] = rf.params_.get_param_float(ACC_X_BIAS);
-    bias[1] = rf.params_.get_param_float(ACC_Y_BIAS);
-    bias[2] = rf.params_.get_param_float(ACC_Z_BIAS);
+    bias[0] = rf.params_.get_param_float(PARAM_ACC_X_BIAS);
+    bias[1] = rf.params_.get_param_float(PARAM_ACC_Y_BIAS);
+    bias[2] = rf.params_.get_param_float(PARAM_ACC_Z_BIAS);
 
     for (int i = 0; i < 3; i++)
     {
